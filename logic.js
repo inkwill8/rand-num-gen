634bc59592a6dfd9d6280e755308daecb95e5471
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-08 11:21:33 -0500

    Add number generation logic
    Added the logic to actually generate random
    numbers and append the value to the input.
    
    Also added footer and updated some styles

--- logic.js
+++ logic.js
@@ -5,12 +5,11 @@ const input = document.querySelector('input');
 const pressMe = document.querySelector('button');
 
 // Add event listener to the button
-pressMe.addEventListener(onClick);
+pressMe.addEventListener('click', onClick);
 
 // Define the function
-function 
-
-// Discovered this while reading 'functions' in MDN webdocs.
-// So, we're using this method to make JS select a random number between 0 and 1
-let random = Math.round(Math.random() * 100);
+function onClick () {
+    let random = Math.round((Math.random() * 100) + 1);
+    input.value = random;
+}
 
