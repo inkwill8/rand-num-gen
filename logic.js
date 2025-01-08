0522157c88c9884283a64e83f9e8920db4e7029e
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-08 11:31:27 -0500

    Add h1 text and change font

--- logic.js
+++ logic.js
@@ -7,7 +7,7 @@ const pressMe = document.querySelector('button');
 // Add event listener to the button
 pressMe.addEventListener('click', onClick);
 
-// Define the function
+// Define the function when clicked
 function onClick () {
     let random = Math.round((Math.random() * 100) + 1);
     input.value = random;
