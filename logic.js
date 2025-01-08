3b0ab90e87df94f207e5df73626f88df25f2bf17
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-07 23:10:36 -0500

    Begin basic log and DOM manipulation

--- logic.js
+++ logic.js
@@ -1,5 +1,16 @@
-// Discovered this while reading 'functions' in MDN webdocs.
+// Find the input on the good ol' DOM
+const input = document.querySelector('input');
 
+// Find button on DOM
+const pressMe = document.querySelector('button');
+
+// Add event listener to the button
+pressMe.addEventListener(onClick);
+
+// Define the function
+function 
+
+// Discovered this while reading 'functions' in MDN webdocs.
+// So, we're using this method to make JS select a random number between 0 and 1
 let random = Math.round(Math.random() * 100);
 
-console.log(random);
